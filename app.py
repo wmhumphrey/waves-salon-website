8ef1ef8b30f32d9f3720038c0316a3d03b3b49db
wmhumphrey <wyatt@humphreyfamily.me>
2025-11-15 15:00:52 -0500

    Initial commit for Waves Salon website

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Make {{ year }} available in all templates
+@app.context_processor
+def inject_year():
+    return {"year": datetime.now().year}
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
